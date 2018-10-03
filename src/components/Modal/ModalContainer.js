37d289d80a7f03242e55f22f13fae1af79e79d99
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-10-02 22:57:37 -0700

    Close modal if click container

--- src/components/Modal/ModalContainer.js
+++ src/components/Modal/ModalContainer.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import Modal from './Modal.js';
 
 class ModalContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('click', this.onClick, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onClick, false);
+  }
+
+  onClick(e) {
+    if(this.props.showModal && e.target.className === "modal-container") {
+      this.props.toggleModal();
+    }
+  }
+
   render() {
     return (
       <Modal 
