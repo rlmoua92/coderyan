c700a5523eb3af11bd34706e43d2bca1ed4dc609
Ryan Moua <rlmoua92@gmail.com>
2018-10-12 00:08:26 -0700

    Move state reset to Container

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,45 +1,38 @@
-import React, { Component} from 'react';
+import React from 'react';
 import NavBar from '../NavBar';
 import GameSettings from '../GameSettings';
 import { Link } from 'react-router-dom';
 import './Home.scss';
 
-class Home extends Component {
-
-  componentDidMount(){
-    this.props.newGame();
-  }
-
-  render() {
-    const {
-      randKey,
-      onRoomKeyChange,
-      onStartGame,
-      gameType,
-    } = this.props;
+const Home = (props) => {
+  const {
+    randKey,
+    onRoomKeyChange,
+    onStartGame,
+    gameType,
+  } = props;
   
-    const rightContent = 
-    <div className="nav-settings">
-      <GameSettings />
-    </div>;
+  const rightContent = 
+  <div className="nav-settings">
+    <GameSettings />
+  </div>;
   
-    return (
-      <div className="flex flex-column height-100">
-        <NavBar rightContent={rightContent} />
-        <div className="home flex v-align-center">
-          <div className="home-content flex flex-column">
-            <h1>.codeRyan</h1>
-            <div className="roomKey">
-              <label>ENTER A ROOM KEY:</label>
-              <input type="text" value={randKey} onChange={onRoomKeyChange} />
-              {randKey.length <= 0 ? <div className="input-error">ERROR: KEY CANNOT BE BLANK</div> : null}
-            </div>
-            <Link to={"/" + gameType + "/" + randKey} className="button flex-100" onClick={onStartGame}>START GAME</Link>
+  return (
+    <div className="flex flex-column height-100">
+      <NavBar rightContent={rightContent} />
+      <div className="home flex v-align-center">
+        <div className="home-content flex flex-column">
+          <h1>.codeRyan</h1>
+          <div className="roomKey">
+            <label>ENTER A ROOM KEY:</label>
+            <input type="text" value={randKey} onChange={onRoomKeyChange} />
+            {randKey.length <= 0 ? <div className="input-error">ERROR: KEY CANNOT BE BLANK</div> : null}
           </div>
+          <Link to={"/" + gameType + "/" + randKey} className="button flex-100" onClick={onStartGame}>START GAME</Link>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Home from './Home.js';
 import { connect } from 'react-redux';
 import { 
@@ -5,6 +6,30 @@ import {
 	homeStartGame,
   newGame,
  } from '../../actions';
+
+class HomeContainer extends Component {
+
+  componentDidMount(){
+    this.props.newGame();
+  }
+
+  render() {
+    const {
+      randKey,
+      gameType,
+      onRoomKeyChange,
+      onStartGame,
+    } = this.props;
+    return (
+      <Home 
+        randKey={randKey}
+        gameType={gameType}
+        onRoomKeyChange={onRoomKeyChange}
+        onStartGame={onStartGame}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => { 
   console.log(state);
@@ -23,4 +48,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Home);
+)(HomeContainer);
