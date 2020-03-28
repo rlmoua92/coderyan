bf494cef2a6236d03766640883360e410b3f9556
Ryan Moua <rlmoua92@gmail.com>
2020-03-28 14:56:58 -0700

    Add turn player color to nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,17 +6,18 @@ const NavBar = (props) => {
     leftContent,
     centerContent,
     rightContent,
+    isPlayerRed,
   } = props;
 
   return (
-    <div className="nav-bar flex v-align-center">
-      <div className="flex v-align-center">
+    <div className="nav-bar flex v-align-stretch">
+      <div className={`nav-bar-content nav-bar__left-container ${leftContent ? isPlayerRed ? 'is-red' : 'is-blue' : ''}`}>
         {leftContent}
       </div>
-      <div className="flex v-align-center">
+      <div className="nav-bar-content nav-bar__center-container flex v-align-center">
         {centerContent}
       </div>
-      <div className="flex v-align-center">
+      <div className="nav-bar-content nav-bar__right-container flex v-align-center">
         {rightContent}
       </div>
     </div>
